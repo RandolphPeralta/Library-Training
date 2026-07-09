d9e01a4f7baa07aeadca17696bb3f823438c0b02
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-08 22:26:40 -0500

    Punto de entrada del proyecto
    Se realizaron cambios en el punto de entrada del proyecto, ya que se tenia una clase para la vista del menú y las opciones, ahora están juntas dentro en una clase.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { Libro } from "./types/Libro";
 import { Estudiante } from "./types/Estudiante";
 import { Prestamo } from "./types/Prestamo";
 import { MenuAccion } from "./ui/terminal/MenuAccion";
-import { ConsoleView } from "./ui/terminal/ConsoleView";
-import { App } from "./app/terminal/app";
+import { App } from "./app/app";
 
 const memoriaLibro = new Memoria<Libro>();
 const memoriaEstudiante = new Memoria<Estudiante>();
@@ -12,7 +11,6 @@ const memoriaPrestamo = new Memoria<Prestamo>();
 
 const menu = new MenuAccion(memoriaEstudiante, memoriaLibro, memoriaPrestamo);
 
-const view = new ConsoleView();
-const app = new App(menu, view);
+const app = new App(menu);
 
 app.run();
