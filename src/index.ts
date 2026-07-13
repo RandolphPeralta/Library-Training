17f030c55450ba27538e9b4a5f59b2180fafb13f
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-13 16:49:55 -0500

    Punto de entrada del proyecto
    Índice o punto de entrada del proyecto

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { MemoryRAM } from "./core/persistence/Memory";
 import { Book } from "./types/Book";
 import { Student } from "./types/Student";
 import { Loan } from "./types/Loan";
-import { MenuConsola } from "./ui/terminal/MenuConsola";
+import { MenuConsole } from "./ui/terminal/MenuConsole";
 import { App } from "./app/app";
 
 const Persistencebook = new MemoryRAM<Book>();
 const Persistencestudent = new MemoryRAM<Student>();
 const Persistenceloan = new MemoryRAM<Loan>();
 
-const menu = new MenuConsola(Persistencestudent, Persistencebook, Persistenceloan);
+const menu = new MenuConsole(Persistencestudent, Persistencebook, Persistenceloan);
 
 const app = new App(menu);
 
