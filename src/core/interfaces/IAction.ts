c55d7dd28004bcb114267d6011ca13de428c0380
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-17 09:13:02 -0500

    Abstracción
    Política de alto nivel de mi proyecto, se realizaron cambios con el nombre de métodos lectura y creación de elementos agnósticos al detalle

--- src/core/interfaces/IAction.ts
+++ src/core/interfaces/IAction.ts
@@ -1,11 +1,11 @@
 export interface ISave<T> {
-  save(some: T): any;
+  create(some: T): any;
   delete(id: any): any;
 }
 
 export interface IAction<T> extends ISave<T>{
   update(some: any): any;
-  show(): T[];
+  read(): T[];
 }
 
 export interface IAdditionalAction<T> extends IAction<T> {
