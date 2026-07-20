07d38eba8aba3fe9374dc1cbf039b4e397c1c800
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-19 21:18:35 -0500

    Eliminación de nuevas interfaces
    Se eliminan interfaces las cuales no son necesarias para mi proyecto

--- src/domain/interfaces/IExtra.ts
+++
@@ -1,6 +0,0 @@
-import { IAdditionalAction } from "./IAction";
-
-export interface IExtraAction<T> extends IAdditionalAction<T> {
-    createbyid(id: any, some: any): any;
-    updatebyid(id: any, some: any): any;
-}
