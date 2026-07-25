72057151cd4e66ad68344a4d5eeafe67d2dd7dea
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-25 07:56:03 -0500

    Revert "índice"
    This reverts commit 218f50ce6d7ab2f4e928ef6cf972c4cd9ef01ef2.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import { Book } from "./entities/types/Book";
-import { Student } from "./entities/types/Student";
-import { Loan } from "./entities/types/Loan";
-import { MemoryRAM } from "./frameworks&drivers/persistence/Memory";
-import { Studentusecase } from "./usescases/studentusecase";
-import { Bookusecase } from "./usescases/bookusecase";
-import { Loanusecase } from "./usescases/loanusecase";
-import { StudentConsole } from "./frameworks&drivers/ui/terminal/consoles/studentconsol";
-import { BookConsole } from "./frameworks&drivers/ui/terminal/consoles/bookconsol";
-import { LoanConsole } from "./frameworks&drivers/ui/terminal/consoles/loanconsol";
-import { MenuConsole } from "./frameworks&drivers/ui/terminal/menu/Menuconsole";
-import { App } from "./app";
+import { Book } from "./app/domain/types/Book";
+import { Student } from "./app/domain/types/Student";
+import { Loan } from "./app/domain/types/Loan";
+import { MemoryRAM } from "./app/infrastructure/persistence/Memory";
+import { Studentservice } from "./app/application/services/studentservice";
+import { Bookservice } from "./app/application/services/bookservice";
+import { Loanservice } from "./app/application/services/loanservice";
+import { StudentConsole } from "./app/ui/terminal/consoles/studentconsol";
+import { BookConsole } from "./app/ui/terminal/consoles/bookconsol";
+import { LoanConsole } from "./app/ui/terminal/consoles/loanconsol";
+import { MenuConsole } from "./app/ui/terminal/menu/Menuconsole";
+import { App } from "./app/app";
 
 const MemoryBook = new MemoryRAM<Book>();
 const MemoryStudent = new MemoryRAM<Student>();
 const MemoryLoan = new MemoryRAM<Loan>();
 
-const studentusecase = new Studentusecase(MemoryStudent);
-const bookusecase = new Bookusecase(MemoryBook);
-const loanusecase = new Loanusecase(MemoryLoan, MemoryBook, MemoryStudent);
+const studentservice = new Studentservice(MemoryStudent);
+const bookservice = new Bookservice(MemoryBook);
+const loanservice = new Loanservice(MemoryLoan, MemoryBook, MemoryStudent);
 
-const studentconsole = new StudentConsole(studentusecase);
-const bookconsole = new BookConsole(bookusecase);
-const loanconsole = new LoanConsole(loanusecase, bookusecase, studentusecase);
+const studentconsole = new StudentConsole(studentservice);
+const bookconsole = new BookConsole(bookservice);
+const loanconsole = new LoanConsole(loanservice, bookservice, studentservice);
 
 const menu = new MenuConsole(studentconsole, bookconsole, loanconsole);
 
