e4e8f20fbcc6ddb75953550fe1aa8416200579ea
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-03 21:11:20 -0500

    Índice
    Punto de entrada de mi proyecto

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { Book } from "./app/domain/types/Book";
 import { Student } from "./app/domain/types/Student";
 import { Loan } from "./app/domain/types/Loan";
 import { MemoryRAM } from "./app/infrastructure/persistences/Memory";
-import { Loanservice } from "./app/application/services/loanservice";
-import { Studentservice } from "./app/application/services/studentservice";
-import { Bookservice } from "./app/application/services/bookservice";
+import { Service } from "./app/application/services/Services";
 import { Studentconsole } from "./app/ui/terminal/consoles/studentconsol";
 import { Bookconsole } from "./app/ui/terminal/consoles/bookconsol";
 import { LoanConsole } from "./app/ui/terminal/consoles/loanconsol";
@@ -15,15 +13,15 @@ const repositorybook = new MemoryRAM<Book>();
 const repositorystudent = new MemoryRAM<Student>();
 const repositoryloan = new MemoryRAM<Loan>();
 
-const loanservice = new Loanservice(repositoryloan, repositorybook, repositorystudent)
-const studentservice = new Studentservice(repositorystudent, repositoryloan)
-const bookservice = new Bookservice(repositorybook)
+const loanservice = new Service<Loan>(repositoryloan);
+const studentservice = new Service<Student>(repositorystudent);
+const bookservice = new Service<Book>(repositorybook);
 
-const studentconsole = new Studentconsole(studentservice);
-const bookconsole = new Bookconsole(bookservice);
-const loanconsole = new LoanConsole(loanservice)
+const studentconsoletest = new Studentconsole(studentservice, loanservice);
+const bookconsoletest = new Bookconsole(bookservice);
+const loanconsole = new LoanConsole(studentservice, bookservice,loanservice);
 
-const menu = new MenuConsole(studentconsole, bookconsole, loanconsole);
+const menu = new MenuConsole(studentconsoletest, bookconsoletest, loanconsole);
 
 const app = new App(menu);
 app.run();
