4c0d4172554cec9dcdf1086df7603530e7c64b22
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-05 21:39:51 -0500

    Services
    Cambios en mis servicios para realizar las comprobaciones correspondientes

--- src/app/application/services/Services.ts
+++ src/app/application/services/Services.ts
@@ -1,10 +1,12 @@
 import { IService } from "../../domain/interfaces/IServices";
+import { IApprobation } from "../../domain/interfaces/IApprobation";
 import { IAddidionalaction } from "../../domain/interfaces/IAction";
 
 export class Service<T> implements IService<T> {
-    constructor(private repository: IAddidionalaction<T>){}
+    constructor(private repository: IAddidionalaction<T>, private approbator: IApprobation<T>) { }
 
     create(item: T) {
+        if (!this.approbator.approve(item)) return false
         return this.repository.create(item);
     }
 
@@ -13,6 +15,7 @@ export class Service<T> implements IService<T> {
     }
 
     update(item: T) {
+        if (!this.approbator.approve(item)) return false
         return this.repository.update(item);
     }
 
