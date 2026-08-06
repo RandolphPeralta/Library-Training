51072c9060ee286dc094fc63d2002d21d51517c4
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-05 21:41:13 -0500

    Índice
    Punto de entrada de mi proyecto

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Student } from "./app/domain/types/Student";
 import { Loan } from "./app/domain/types/Loan";
 import { MemoryRAM } from "./app/infrastructure/persistences/Memory";
 import { Service } from "./app/application/services/Services";
+import { Approbation } from "./app/application/approbation/Approbation";
 import { Studentconsole } from "./app/ui/terminal/consoles/studentconsol";
 import { Bookconsole } from "./app/ui/terminal/consoles/bookconsol";
 import { LoanConsole } from "./app/ui/terminal/consoles/loanconsol";
@@ -13,13 +14,17 @@ const repositorybook = new MemoryRAM<Book>();
 const repositorystudent = new MemoryRAM<Student>();
 const repositoryloan = new MemoryRAM<Loan>();
 
-const loanservice = new Service<Loan>(repositoryloan);
-const studentservice = new Service<Student>(repositorystudent);
-const bookservice = new Service<Book>(repositorybook);
+const studentapprobator = new Approbation<Student>();
+const bookapprobator = new Approbation<Book>();
+const loanapprobator = new Approbation<Loan>();
+
+const loanservice = new Service<Loan>(repositoryloan, loanapprobator);
+const studentservice = new Service<Student>(repositorystudent, studentapprobator);
+const bookservice = new Service<Book>(repositorybook, bookapprobator);
 
 const studentconsole = new Studentconsole(studentservice, loanservice);
 const bookconsole = new Bookconsole(bookservice);
-const loanconsole = new LoanConsole(studentservice, bookservice,loanservice);
+const loanconsole = new LoanConsole(studentservice, bookservice, loanservice);
 
 const menu = new MenuConsole(studentconsole, bookconsole, loanconsole);
 
