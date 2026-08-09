be43d020304e6ef2a99d67effc361e712d68e209
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-09 12:32:17 -0500

    Validation
    Cambios en los tipos de retorno de mis validaciones

--- src/app/application/validations/Validation.ts
+++ src/app/application/validations/Validation.ts
@@ -1,7 +1,7 @@
 import { IValidation } from "../../domain/interfaces/IValidation";
 
-export class Validation<T> implements IValidation<T> {
-    validate(item: any) {
+export class Validation<T extends object> implements IValidation<T> {
+    validate(item: T): boolean {
     for(const value of Object.values(item)){
         if(value === "" || value === null || value === undefined){
             return false;
