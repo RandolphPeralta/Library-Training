8a3aed73ca7625fece8a6e6ddb77eaa610a73866
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-13 14:29:40 -0500

    Actualización en IView
    Se realizo la extensión de la abstracción por parte de la vista para evitar referencia circular en las clases

--- src/app/domain/interfaces/IView.ts
+++ src/app/domain/interfaces/IView.ts
@@ -1,3 +1,19 @@
 export interface IView {
-  execute(): any;
+  execute(): void;
+}
+
+export interface IMenuview extends IView {
+
+}
+
+export interface IStudentview extends IView {
+
+}
+
+export interface IBookview extends IView {
+    
+}
+
+export interface ILoanview extends IView {
+    
 }
