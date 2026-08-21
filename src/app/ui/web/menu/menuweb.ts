f9df407ca55761288fc48e44bda51b6d8306919e
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-08-21 18:37:24 -0500

    UI
    Actualizacion del menuweb.ts, ya que se agrego un mensaje de bienvenida

--- src/app/ui/web/menu/menuweb.ts
+++ src/app/ui/web/menu/menuweb.ts
@@ -6,76 +6,47 @@ export class Menuweb implements IMenuview {
         private studentMenu: IStudentview,
         private bookMenu: IBookview,
         private loanMenu: ILoanview
-    ) {}
+    ) { }
 
     execute(): void {
 
-        const btnStudents =
-            document.getElementById("menuStudents")!;
-
-        const btnBooks =
-            document.getElementById("menuBooks")!;
-
-        const btnLoans =
-            document.getElementById("menuLoans")!;
-
+        const btnStudents = document.getElementById("menuStudents")!;
+        const btnBooks = document.getElementById("menuBooks")!;
+        const btnLoans = document.getElementById("menuLoans")!;
 
         btnStudents.addEventListener("click", (event) => {
-
             event.preventDefault();
-
             this.showView("studentView");
-
             this.studentMenu.execute();
         });
 
-
         btnBooks.addEventListener("click", (event) => {
-
             event.preventDefault();
-
             this.showView("bookView");
-
             this.bookMenu.execute();
         });
 
-
         btnLoans.addEventListener("click", (event) => {
-
             event.preventDefault();
-
             this.showView("loanView");
-
             this.loanMenu.execute();
         });
 
-
-        this.showView("studentView");
-
-        this.studentMenu.execute();
+        this.showView("blankView");
     }
 
-
     private showView(viewId: string): void {
-
         const views = [
+            "blankView",
             "studentView",
             "bookView",
             "loanView"
         ];
 
-
         views.forEach(id => {
-
-            document
-                .getElementById(id)!
-                .classList.add("d-none");
-
+            document.getElementById(id)!.classList.add("d-none");
         });
 
-
-        document
-            .getElementById(viewId)!
-            .classList.remove("d-none");
+        document.getElementById(viewId)!.classList.remove("d-none");
     }
 }
